a0361f57442d622bb6b2128ac2c0a54f9c68e97d
Ankita <ankita@ajency.in>
2019-05-07 06:49:41 +0530

    updated capabilities page

--- wp-content/themes/ddeurope/capabilities.php
+++ wp-content/themes/ddeurope/capabilities.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	$default_sidebar_position = get_theme_mod( 'default_sidebar_position', 'right' );
 ?>
 
-<div class="gallery">
+<div class="capabilities">
 	<div class="header_image">
 		<div class="header">
 			<div class="container">
@@ -18,17 +18,46 @@ get_header(); ?>
 				<div class="breadcrumbs-wrapper">
       				<div class="breadcrumbs-inside">
   						<a href="#">Home</a> <span class="sep-icon"><i class="fa fa-angle-right"></i></span> <span>capabilities</span>
-				<h2 class="title">Gallery</h2>
-				<div class="breadcrumbs-wrapper">
-      				<div class="breadcrumbs-inside">
-  						<a href="#">Home</a> <span class="sep-icon"><i class="fa fa-angle-right"></i></span> <span>Gallery</span>
-  					</div>
-  				</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
-	<div class="gallery_content">
+	<div class="capability_content">
 		<div class="container">
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12">
+				<p>INNOVATIVE DESIGN</p>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+				<p>PERSONALISED ATTENTION</p>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12">
+				<p>MODERN PRODUCTION</p>
+				</div>
+			</div>
+			<hr>
+			<div class="row">
+				<div class="col-lg-6 col-md-12 col-sm-12">
+					<p><strong>Design Expertise</strong></p>
+						<p>Our design team comprises of 3d &amp; 2d designers specializing in Exhibition design. They bring combined experience of over 50 years in Design.</p>
+						<p>For every Design we listen to you with intensity and understand your requirement without jumping into specifics. What are you exhibiting? For whom? With what objectives? These are the questions whose answers we translate into design.</p>
+
+
+					<p><strong>Personalized Client Service</strong></p>	
+						<p>We deliver our customer experience through dedicated team members who liaise with the Client to ensure that every detail is taken care promptly.</p>
+						<p>A Client Service person is designated as a single point of contact to understand &amp; attend to Client requirements. This ensures ease of handling, personal attention to details and the convenience of not having to brief people repeatedly.</p>
+						<p>We offer the expertise of a premier exhibition company with a level of personalised attention that distinguishes us.</p>		
+												
+					<p><strong>Modern & efficient production facilities</strong></p>
+						<p>With over 20,000 square feet of production facility that is well equipped with the latest in fabrication equipment, we have the capacity and the infrastructure to handle large volumes of exhibition builds. We have successfully handled multiple builds without any bottlenecking.</p>
+						<p>The company has a policy of in-house fabrication and our stands are built at our production premises. This ensures Quality of the build as well as its timely delivery, the two key aspects of our Service. We are also adequately stocked with furniture &amp; electricals to cater to our build requirements.</p>
+						<p>Every year we execute over 125 exhibition stands across India &amp; the world.</p>
+
+				</div>
+				<div class="col-lg-6 col-md-12 col-sm-12">
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
