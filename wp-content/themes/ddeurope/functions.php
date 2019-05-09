09fe65a16fa488eab037680e0d7376b442f60db1
Jeromie <jeromie@ajency.in>
2019-05-09 15:53:49 +0530

    extra jquery removed

--- wp-content/themes/ddeurope/functions.php
+++ wp-content/themes/ddeurope/functions.php
@@ -9,7 +9,7 @@ function enqueue_theme_styles() {
 add_action( 'wp_enqueue_scripts', 'enqueue_theme_styles' );
 
 function enqueue_theme_scripts() { 
-    wp_register_script( 'jquery-js', get_template_directory_uri().'/assets/js/jquery.js' , '', '', true );
+    // wp_register_script( 'jquery-js', get_template_directory_uri().'/assets/js/jquery.js' , '', '', true );
     wp_register_script( 'bootstrap-js', get_template_directory_uri().'/assets/js/bootstrap.min.js' , '', '', true );
     wp_register_script( 'simplyscroll-js', get_template_directory_uri().'/assets/js/jquery.simplyscroll.js' , '', '', true );
     wp_register_script( 'flickity-js', get_template_directory_uri().'/assets/js/flickity.pkgd.min.js', array(), '', true);
@@ -18,7 +18,7 @@ function enqueue_theme_scripts() {
 
     wp_register_script( 'themescripts-js', get_template_directory_uri().'/assets/js/theme-scripts.js' , '', '', true );
 
-    wp_enqueue_script('jquery-js');
+    // wp_enqueue_script('jquery-js');
     wp_enqueue_script('bootstrap-js');
     wp_enqueue_script('simplyscroll-js');
     wp_enqueue_script('flickity-js'); 
