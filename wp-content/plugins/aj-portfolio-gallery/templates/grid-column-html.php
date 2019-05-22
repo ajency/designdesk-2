7128c0a497c251117f92b83ecb8454e2a630f9e7
Ankita <ankita@ajency.in>
2019-05-22 18:58:37 +0530

    onclick open particular album

--- wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
+++ wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
@@ -1,5 +1,5 @@
 <div class="col-lg-4 col-md-6 col-sm-12 img-block">
-  <a href="#" class="pg-wrapper <?php echo $lightbox == true ? 'prettyphoto-btn' : ''  ?>" data-galid="<?php echo get_the_ID(); ?>">
+  <a href="./demo/?album=<?php echo get_the_ID(); ?>" class="pg-wrapper <?php echo $lightbox == true ? 'prettyphoto-btn' : ''  ?>" data-galid="<?php echo get_the_ID(); ?>">
     <div class="pg-thumbnail" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)"></div>
     <div class="pg-title"><label><?php the_title(); ?></label></div>
   </a>
