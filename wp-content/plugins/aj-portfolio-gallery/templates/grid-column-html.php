48721498f6c22083948ce8aad0ee3651d7c51f64
Ankita <ankita@ajency.in>
2019-05-22 15:04:32 +0530

    sprite.png added

--- wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
+++ wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
@@ -1,5 +1,5 @@
 <div class="col-lg-4 col-md-6 col-sm-12 img-block">
-  <a href="#<?php echo get_the_ID(); ?>" class="pg-wrapper <?php  echo $lightbox == true ? 'prettyphoto-btn' : ''  ?>" data-galid="<?php echo get_the_ID(); ?>">
+  <a href="#" class="pg-wrapper <?php  echo $lightbox == true ? 'prettyphoto-btn' : ''  ?>" data-galid="<?php echo get_the_ID(); ?>">
     <div class="pg-thumbnail" style="background-image: url(<?php echo get_the_post_thumbnail_url(  $post_id, array(300, 250) ); ?>)"></div>
     <div class="pg-title"><label><?php the_title(); ?></label></div>
   </a>
