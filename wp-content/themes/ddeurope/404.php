111ce9968b1ef12eca6c1d5ae9a56e065678e2df
Ankita <ankita@ajency.in>
2019-05-24 16:51:29 +0530

    Added 404 file

---
+++ wp-content/themes/ddeurope/404.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Template Name: 404 page
+ */
+
+get_header(); ?>
+
+   <div id="main-content">
+   		<div class="container">
+			<h2 class="page-title">File not Found</h2>
+			<h4 class="page-subtitle">Sorry, but we couldn't find the content you were looking for.</h4>
+			<div class="back_home w-auto">
+				<a href="https://dd-europe.com/" target="_blank"> Back To Home</a>
+			</div>
+   		</div>
+   	</div>
+
+<?php get_footer(); ?>
