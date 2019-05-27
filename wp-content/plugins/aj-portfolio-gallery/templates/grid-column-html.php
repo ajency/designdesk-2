7b4c93d2c8ae8b9bec82132f093fe59dc9302160
Ankita <ankita@ajency.in>
2019-05-27 12:00:08 +0530

    remove unused CSS

--- wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
+++ wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4 col-md-6 col-sm-12 img-block">
   <a href="<?php echo ($lightbox == 'true') ? 'javascript.void(0);' : './exhibition-stands-gallery/?album='.get_the_ID(); ?>" class="pg-wrapper <?php echo ($lightbox == 'true') ? 'prettyphoto-btn' : ''; ?>" data-galid="<?php echo get_the_ID(); ?>">
-    <div class="pg-thumbnail" style="background-image: url(<?php echo get_the_post_thumbnail_url(); ?>)"></div>
+    <div class="pg-thumbnail" style="background-image: url(<?php echo get_the_post_thumbnail_url( $post_ID , 'large'); ?>)"></div>
     <div class="pg-title"><label><?php the_title(); ?></label></div>
   </a>
 </div>
