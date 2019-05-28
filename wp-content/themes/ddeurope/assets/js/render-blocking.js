d4e9b9dd215516ab59330b97e284c1abf43fcd48
Ankita <ankita@ajency.in>
2019-05-28 10:01:42 +0530

    Eliminate render-blocking resources

---
+++ wp-content/themes/ddeurope/assets/js/render-blocking.js
@@ -0,0 +1,17 @@
+<script type="text/javascript">
+function parseJSAtOnload() {
+var links = ["jquery.waypoints.min.js", "jquery-migrate.min.js", "lightgallery-all.min.js"],
+headElement = document.getElementsByTagName("head")[0],
+linkElement, i;
+for (i = 0; i < links.length; i++) {
+linkElement = document.createElement("script");
+linkElement.src = links[i];
+headElement.appendChild(linkElement);
+}
+}
+if (window.addEventListener)
+window.addEventListener("load", parseJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", parseJSAtOnload);
+else window.onload = parseJSAtOnload;
+</script>
