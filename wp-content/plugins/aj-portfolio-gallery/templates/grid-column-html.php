407e6280cbc0f6b05d0edb998377166f55102c8e
Ankita <ankita@ajency.in>
2019-06-07 10:27:42 +0530

    added text on hover

--- wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
+++ wp-content/plugins/aj-portfolio-gallery/templates/grid-column-html.php
@@ -1,6 +1,9 @@
 <div class="col-lg-4 col-md-6 col-sm-12 img-block">
-  <a href="<?php echo ($lightbox == 'true') ? 'javascript.void(0);' : './exhibition-stands-gallery/?album='.get_the_ID(); ?>" class="pg-wrapper <?php echo ($lightbox == 'true') ? 'prettyphoto-btn' : ''; ?>" data-galid="<?php echo get_the_ID(); ?>">
+  <a href="<?php echo ($lightbox == 'true') ? 'javascript.void(0);' : './exhibition-stands-gallery/?album='.get_the_ID();  ?> " class="pg-wrapper <?php echo ($lightbox == 'true') ? 'prettyphoto-btn' : ''; ?>" data-galid="<?php echo get_the_ID(); ?>">
     <div id="bg-image" class="pg-thumbnail lazy" style="background-image: url(<?php echo get_the_post_thumbnail_url( $post_ID , 'large'); ?>)"></div>
-    <div class="pg-title"><label><?php the_title(); ?></label></div>
+    <div class="pg-title"><label><?php the_title(); ?></label>
+    	<p class="location"> <?php echo get_post_meta(get_the_id(), 'aj_location', true); ?> </p>
+		<p class="area"> <?php echo get_post_meta(get_the_id(), 'aj_area', true); ?> </p>
+    </div>
   </a>
 </div>
