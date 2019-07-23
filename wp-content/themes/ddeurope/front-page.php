bb70354314f48e9645dd2ae44ddb70a43bebdfff
Ankita <ankita@ajency.in>
2019-07-23 11:40:45 +0530

    break point changed for flat world section

--- wp-content/themes/ddeurope/front-page.php
+++ wp-content/themes/ddeurope/front-page.php
@@ -63,7 +63,7 @@ if ( have_posts() ) {
                 <div class="modal-dialog modal-dialog-centered" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title text-primary" id="exampleModalLongTitle">Contact now for a free proposal
+                            <h5 class="modal-title text-primary" id="exampleModalLongTitle">Contact Us
                             </h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
