2cea19388e865ff3165b271db97172683314a683
Ankita <ankita@ajency.in>
2019-07-23 15:59:36 +0530

    added links

--- wp-content/themes/ddeurope/single.php
+++ wp-content/themes/ddeurope/single.php
@@ -54,17 +54,19 @@ get_header();
 								<?php if ( $wpb_all_query->have_posts() ) : ?>
 									<!-- the loop -->
 									<?php while ( $wpb_all_query->have_posts() ) : $wpb_all_query->the_post(); ?>
-										<div class="col-lg-4 col-md-4 col-sm-12">
+										<div class="col-lg-4 col-md-4 col-sm-12 mb-4">
 											<div class="recent-post_featured-img">
-												<?php
-												if ( has_post_thumbnail() ) :
-												the_post_thumbnail( '' );
-												endif;
-												?>
+												<a href="<?php the_permalink(); ?>">
+													<?php
+													if ( has_post_thumbnail() ) :
+													the_post_thumbnail( '' );
+													endif;
+													?>
+												</a>
 											</div>
 											<div class="recent-post-bg">
 												<div class="recent-post_header my-4">
-													<h2 class="entry-title"><?php the_title(); ?></h2>
+													<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 													<span class="entry-date"><?php echo get_the_date(); ?></span>
 												</div>
 												<p><?php echo wp_trim_words(get_the_content(), 15, '...'); ?></p>
